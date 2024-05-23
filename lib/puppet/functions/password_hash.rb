8bb9641fbe913f8186d06b4905aec571d8214fe5
Paul Reed <paul.reed@puppet.com>
2024-05-23 15:26:30 -0400

    Remove require for bcrypt due to load error (is it included in base jruby?)

--- lib/puppet/functions/password_hash.rb
+++ lib/puppet/functions/password_hash.rb
@@ -1,6 +1,5 @@
 # Function to hash a password for prometheus node exporter
 Puppet::Functions.create_function(:'prometheus_node_exporter::password_hash') do
-  require 'bcrypt'
   dispatch :func do
     required_param 'String', :password
     return_type 'String'
